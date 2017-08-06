6163d7a8200b5462882ab7a5f33c8e5c548f12cb
ChaseLEngel <chaselengel@gmail.com>
2017-08-06 11:01:40 -0700

    Orders are retrieved from api backend.

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,9 +1,10 @@
 <html>
   <head>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.2/vue.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.2/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue-resource@1.3.4"></script>
 
-  <script text="text/javascript" src="{{ URL::asset('js/serviceworker/register.js') }}"></script>
-  <title>Orders</title>
+    <script text="text/javascript" src="{{ URL::asset('js/serviceworker/register.js') }}"></script>
+    <title>Orders</title>
   </head>
 
   <body>
@@ -25,38 +26,34 @@
 
   <script>
 
-    var orders = [
-      {
-        item: 'Hat',
-        quantity: 10,
-        address: '1234 Lake St.',
-        driver_id: 13
-      },
-      {
-        item: 'Shoe',
-        quantity: 8,
-        address: '4835 Blake Rd.',
-        driver_id: 4
-      },
-      {
-        item: 'Shirt',
-        quantity: 2,
-        address: '3843 Lane St.',
-        driver_id: 3
-      }
-    ]
 
     new Vue({
       el: '#app',
+
       data: function() {
-          return { orders: orders }
+          return { orders: [] }
       },
+
+      created: function() {
+        this.fetchOrders();
+      },
+
+      methods: {
+        fetchOrders: function() {
+          this.$http.get('api/orders').then(response => {
+            this.orders = response.body;
+            }, response => {
+              console.error("Failed to get orders from api.")
+            });
+        }
+      },
+
       components: {
         order: {
-          template: "#order-template",
+          template: '#order-template',
           props: ['order']
+          }
         }
-      }
     });
   </script>
 </html>
